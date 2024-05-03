9adb0131b594d1060c1a0b4470e630098ee64065
DerekSturm263 <dereksturm263@gmail.com>
2024-05-03 12:16:58 -0700

    Update SubWeaponTemplate.qtn.cs

--- quantum_code/quantum.code/Serializable Item/Build/Equipment/Weapons/Sub-weapon/Sub-Weapon Template/SubWeaponTemplate.qtn.cs
+++ quantum_code/quantum.code/Serializable Item/Build/Equipment/Weapons/Sub-weapon/Sub-Weapon Template/SubWeaponTemplate.qtn.cs
@@ -5,5 +5,9 @@ namespace Quantum
     public unsafe abstract partial class SubWeaponTemplate
     {
         public FP EnergyAmount;
+
+        public abstract void OnSpawn(Frame f, EntityRef user, EntityRef subWeaponInstance);
+        public virtual void OnUpdate(Frame f, EntityRef user, EntityRef target, EntityRef subWeaponInstance) { }
+        public abstract void OnDespawn(Frame f, EntityRef user, EntityRef subWeaponInstance);
     }
 }
