0c274e48ea5aea2576795d66950238a8c30ed686
DerekSturm263 <dereksturm263@gmail.com>
2024-05-03 12:24:59 -0700

    Update SubWeaponSystem.cs

--- quantum_code/quantum.code/Serializable Item/Build/Equipment/Weapons/Sub-weapon/SubWeaponSystem.cs
+++ quantum_code/quantum.code/Serializable Item/Build/Equipment/Weapons/Sub-weapon/SubWeaponSystem.cs
@@ -4,13 +4,14 @@ namespace Quantum
         {
                 public struct Filter
                 {
-                      public EntityRef Entity;
-                      public SubWeaponInstance* SubWeaponInstance;
+                       public EntityRef Entity;
+                       public SubWeaponInstance* SubWeaponInstance;
                 }
 
                 public override void Update(Frame f, ref Filter filter)
                 {
-                        filter.SubWeaponInstance->
+                        SubWeaponTemplate template = f.FindAsset<SubWeaponTemplate>(filter.SubWeaponInstance->SubWeapon.Template.Id);
+                        template.OnUpdate(f, filter.SubWeaponInstance->User, filter.SubWeaponInstance->Target, filter.Entity);
                 }
         }
 }
