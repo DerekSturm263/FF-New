1c322d5eab7ec693fd43255780c86f09114a6e47
DerekSturm263 <dereksturm263@gmail.com>
2024-05-03 13:11:47 -0700

    Update SubWeaponSystem.cs

--- quantum_code/quantum.code/Serializable Item/Build/Equipment/Weapons/Sub-weapon/SubWeaponSystem.cs
+++ quantum_code/quantum.code/Serializable Item/Build/Equipment/Weapons/Sub-weapon/SubWeaponSystem.cs
@@ -13,5 +13,29 @@ namespace Quantum
                         SubWeaponTemplate template = f.FindAsset<SubWeaponTemplate>(filter.SubWeaponInstance->SubWeapon.Template.Id);
                         template.OnUpdate(f, filter.SubWeaponInstance->User, filter.SubWeaponInstance->Target, filter.Entity);
                 }
+
+                public static void Spawn(Frame f, SubWeapon subWeapon, PlayerLink* player)
+                {
+                        SubWeaponTemplate template = f.FindAsset<SubWeaponTemplate>(subWeapon.Template.Id);
+                        EntityRef entity = f.Create(template.Prototype);
+
+                        if (f.Unsafe.TryGetPointer(entity, out Transform2D* transform))
+                        {
+                                if (f.Unsafe.TryGetPointer(player->Entity, out Stats* stats))
+                                {
+                                        var dict = f.ResolveDictionary(stats->Hurtboxes);
+
+                                        if (f.Unsafe.TryGetPointer(dict[HurtboxType.RightHand], out Transform2D* parentTransform))
+                                        {
+                                                transform>Position = parentTransform->Position;
+                                        }
+
+                                        if (f.Unsafe.TryGetPointer(entity, out SubWeaponInstance* subWeaponInstance))
+                                                subWeaponInstance->Owner = player->Entity;
+                                        
+                                        template.OnSpawn(f, player->Entity, entity);
+                                }
+                        }
+                }
         }
 }
