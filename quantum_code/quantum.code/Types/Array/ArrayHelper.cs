1798270e59ce5c7544f924fa6119beff83050728
DerekSturm263 <dereksturm263@gmail.com>
2024-06-07 09:59:02 -0700

    Update ArrayHelper.cs

--- quantum_code/quantum.code/Types/Array/ArrayHelper.cs
+++ quantum_code/quantum.code/Types/Array/ArrayHelper.cs
@@ -4,7 +4,7 @@ namespace Quantum.Types
 {
     public static class ArrayHelper
     {
-        public static FPVector2 Get(Array4FPVector2 array, int index)
+        public static FPVector2 Get(ArrayPlayerSpawnPoints array, int index)
         {
             return index switch
             {
@@ -12,6 +12,74 @@ namespace Quantum.Types
                 1 => array.Item2,
                 2 => array.Item3,
                 3 => array.Item4,
+                _ => default
+            };
+        }
+        
+        public static FPVector2 Get(ArrayItemSpawnPoints array, int index)
+        {
+            return index switch
+            {
+                0 => array.Item1,
+                1 => array.Item2,
+                2 => array.Item3,
+                3 => array.Item4,
+                4 => array.Item5,
+                5 => array.Item6,
+                6 => array.Item7,
+                7 => array.Item8,
+                8 => array.Item9,
+                9 => array.Item10,
+                10 => array.Item11,
+                11 => array.Item12,
+                12 => array.Item13,
+                13 => array.Item14,
+                14 => array.Item15,
+                15 => array.Item16,
+                _ => default
+            };
+        }
+        
+        public static FPVector2 Get(ArrayItem array, int index)
+        {
+            return index switch
+            {
+                0 => array.Item1,
+                1 => array.Item2,
+                2 => array.Item3,
+                3 => array.Item4,
+                4 => array.Item5,
+                5 => array.Item6,
+                6 => array.Item7,
+                7 => array.Item8,
+                8 => array.Item9,
+                9 => array.Item10,
+                10 => array.Item11,
+                11 => array.Item12,
+                12 => array.Item13,
+                13 => array.Item14,
+                14 => array.Item15,
+                15 => array.Item16,
+                _ => default
+            };
+        }
+        
+        public static FPVector2 Get(ArrayStage array, int index)
+        {
+            return index switch
+            {
+                0 => array.Item1,
+                1 => array.Item2,
+                2 => array.Item3,
+                3 => array.Item4,
+                4 => array.Item5,
+                5 => array.Item6,
+                6 => array.Item7,
+                7 => array.Item8,
+                8 => array.Item9,
+                9 => array.Item10,
+                10 => array.Item11,
+                11 => array.Item12,
                 _ => default
             };
         }
